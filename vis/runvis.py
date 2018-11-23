58abbe8c53cf1519bc0b80ca5c66e45b191e83f3
hsfzxjy <hsfzxjy@163.com>
2018-11-24 01:23:25 +0800

    p

--- vis/runvis.py
+++ vis/runvis.py
@@ -3,15 +3,21 @@ import glob
 import os.path as osp
 
 
-def run_task(input_fn):
+def get_param(input_fn, layer):
 
     _, dir, name = input_fn.split('/')
     os.makedirs(osp.join('vis_output', dir, name), 0o777, True)
     path = osp.join('vis_output', dir, name)
-    print(input_fn, path)
+    return ['-i', osp.abspath(input_fn), '-p', osp.abspath(path), '-l', layer]
 
 
 if __name__ == '__main__':
 
+    params = []
     for fn in glob.glob('vis_input/**/*'):
-        run_task(fn)
+        params.append(get_param(fn, 5))
+    for fn in glob.glob('vis_input/**/*'):
+        params.append(get_param(fn, 4))
+
+    splitted = [params[i:i + 5] for i in range(0, len(params), 5)]
+    print(splitted)

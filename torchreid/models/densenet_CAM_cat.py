78d7af39a114ac87d021a18b3269bb15b5209942
hsfzxjy <hsfzxjy@163.com>
2018-11-28 13:10:31 +0800

    p

--- torchreid/models/densenet_CAM_cat.py
+++ torchreid/models/densenet_CAM_cat.py
@@ -218,7 +218,7 @@ b_channels = sorted(list(set(range(1023)) - set(channels)))
 
 
 class DANetHead(nn.Module):
-    def __init__(self, in_channels, out_channels, norm_layer):
+    def __init__(self, in_channels, out_channels, norm_layer, type_):
         super(DANetHead, self).__init__()
         inter_channels = in_channels // 4
         # inter_channels = in_channels
@@ -243,18 +243,23 @@ class DANetHead(nn.Module):
         self.conv7 = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(inter_channels, out_channels, 1))
 
         self.conv8 = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(inter_channels, out_channels, 1))
+        self.type_ = type_.lower()
+        assert self.type_ in 'cp'
 
     def forward(self, x):
-        # feat1 = self.conv5a(x)
-        # sa_feat, _ = self.sa(feat1)
-        # sa_conv = self.conv51(sa_feat)
-        # sa_output = self.conv6(sa_conv)
 
-        feat2 = self.conv5c(x)
-        sc_feat = self.sc(feat2)
-        sc_conv = self.conv52(sc_feat)
-        sc_output = self.conv7(sc_conv)
-        return sc_output
+        if self.type_ == 'p':
+            feat1 = self.conv5a(x)
+            sa_feat, _ = self.sa(feat1)
+            sa_conv = self.conv51(sa_feat)
+            sa_output = self.conv6(sa_conv)
+            return sa_output
+        else:
+            feat2 = self.conv5c(x)
+            sc_feat = self.sc(feat2)
+            sc_conv = self.conv52(sc_feat)
+            sc_output = self.conv7(sc_conv)
+            return sc_output
         # feat_sum = sa_conv + sc_conv
 
         # sasc_output = self.conv8(feat_sum)
@@ -267,7 +272,7 @@ class DANetHead(nn.Module):
 
 class DensenetCAMCat(densenet_.DenseNet):
 
-    def __init__(self, num_classes, loss, fc_dims, **kwargs):
+    def __init__(self, num_classes, loss, fc_dims, cluster=True, **kwargs):
 
         super().__init__(
             num_classes=num_classes,
@@ -280,43 +285,50 @@ class DensenetCAMCat(densenet_.DenseNet):
             **kwargs
         )
 
-        self.ca1 = DANetHead(len(channels), len(channels), nn.BatchNorm2d)
-        self.ca2 = DANetHead(len(b_channels), len(b_channels), nn.BatchNorm2d)
-        self.fc = self._construct_fc_layer(fc_dims, self.feature_dim * 2, dropout_p=None)
+        self.cluster = cluster
+        self.ca1 = DANetHead(len(channels), len(channels), nn.BatchNorm2d, type_='c')
+        self.ca2 = DANetHead(len(b_channels), len(b_channels), nn.BatchNorm2d, type_='c')
+        self.ca = DANetHead(1024, 1024, nn.BatchNorm2d, type_='c')
+        self.pa = DANetHead(1024, 1024, nn.BatchNorm2d, type_='p')
+        self.fc = self._construct_fc_layer(fc_dims, self.feature_dim * 3, dropout_p=None)
         print(self.fc)
         # feature_dim changed after _construct_fc_layer, so we must construct
         # classifier again
         self.classifier = nn.Linear(self.feature_dim, num_classes)
+        self.ca_avgpool = nn.AdaptiveAvgPool2d(1)
         self.pa_avgpool = nn.AdaptiveAvgPool2d(1)
-
-        import os
-        x = os.environ.get('DAN_part', 'p')
-        if x.lower() not in 'spc':
-            x = 'p'
-        x = x.lower()
-        self.DAN_part = x
-        print('Using part', x)
 
     def forward(self, x):
         f = self.features(x)
         old_f = f
 
+        if self.cluster:
+
+            ca = f
+            c_tensor = torch.tensor(channels).to(torch.device('cuda'))
+            bc_tensor = torch.tensor(b_channels).to(torch.device('cuda'))
+
+            # x1 = torch.index_select(x, 1, c_tensor)
+            # x2 = torch.index_select(x, 1, bc_tensor)
+            # print(x.shape)
+            x1 = ca[:, c_tensor]
+            x2 = ca[:, bc_tensor]
+
+            x1 = self.ca1(x1)
+            x2 = self.ca2(x2)
+
+            ca[:, c_tensor] = x1
+            ca[:, bc_tensor] = x2
+
+        else:
+
+            ca = f
+            ca = self.ca(f)
+        ca = self.ca_avgpool(ca)
+        ca = ca.view(ca.size(0), -1)
+
         pa = f
-        c_tensor = torch.tensor(channels).to(torch.device('cuda'))
-        bc_tensor = torch.tensor(b_channels).to(torch.device('cuda'))
-
-        # x1 = torch.index_select(x, 1, c_tensor)
-        # x2 = torch.index_select(x, 1, bc_tensor)
-        # print(x.shape)
-        x1 = pa[:, c_tensor]
-        x2 = pa[:, bc_tensor]
-
-        x1 = self.ca1(x1)
-        x2 = self.ca2(x2)
-
-        pa[:, c_tensor] = x1
-        pa[:, bc_tensor] = x2
-
+        pa = self.pa(f)
         pa = self.pa_avgpool(pa)
         pa = pa.view(pa.size(0), -1)
 
@@ -325,7 +337,7 @@ class DensenetCAMCat(densenet_.DenseNet):
 
         v = v.view(v.size(0), -1)
 
-        v = torch.cat((v, pa), 1)
+        v = torch.cat((v, pa, ca), 1)
         # v = pa
         if self.fc is not None:
             v = self.fc(v)
@@ -343,12 +355,13 @@ class DensenetCAMCat(densenet_.DenseNet):
             # raise KeyError("Unsupported loss: {}".format(self.loss))
 
 
-def densenet121_CAM_cat(num_classes, loss, pretrained='imagenet', **kwargs):
+def densenet121_CAM_cl_cat(num_classes, loss, pretrained='imagenet', **kwargs):
 
     model = DensenetCAMCat(
         num_classes=num_classes,
         loss=loss,
         fc_dims=None,
+        cluster=True,
         **kwargs
     )
 
@@ -358,12 +371,46 @@ def densenet121_CAM_cat(num_classes, loss, pretrained='imagenet', **kwargs):
     return model
 
 
-def densenet121_CAM_cat_fc512(num_classes, loss, pretrained='imagenet', **kwargs):
+def densenet121_CAM_cl_cat_fc512(num_classes, loss, pretrained='imagenet', **kwargs):
 
     model = DensenetCAMCat(
         num_classes=num_classes,
         loss=loss,
         fc_dims=[512],
+        cluster=True,
+        **kwargs
+    )
+
+    if pretrained == 'imagenet':
+        densenet_.init_pretrained_weights(model, densenet_.model_urls['densenet121'])
+
+    return model
+
+
+#
+def densenet121_CAM_noncl_cat(num_classes, loss, pretrained='imagenet', **kwargs):
+
+    model = DensenetCAMCat(
+        num_classes=num_classes,
+        loss=loss,
+        fc_dims=None,
+        cluster=False,
+        **kwargs
+    )
+
+    if pretrained == 'imagenet':
+        densenet_.init_pretrained_weights(model, densenet_.model_urls['densenet121'])
+
+    return model
+
+
+def densenet121_CAM_noncl_cat_fc512(num_classes, loss, pretrained='imagenet', **kwargs):
+
+    model = DensenetCAMCat(
+        num_classes=num_classes,
+        loss=loss,
+        fc_dims=[512],
+        cluster=False,
         **kwargs
     )
 

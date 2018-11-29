de4fab4c6a3e748ed5860c81a2f920318a1cf083
hsfzxjy <hsfzxjy@163.com>
2018-11-30 00:59:19 +0800

    p

--- train_imgreid_xent.py
+++ train_imgreid_xent.py
@@ -183,7 +183,7 @@ def train(epoch, model, criterion, optimizer, trainloader, use_gpu, fixbase=Fals
             imgs, pids = imgs.cuda(), pids.cuda()
 
         outputs = model(imgs)
-        print(type(outputs), len(outputs))
+        print(type(outputs), len(outputs), type(pids), len(pids))
         if isinstance(outputs, (tuple, list)):
             loss = DeepSupervision(criterion, outputs, pids)
         else:

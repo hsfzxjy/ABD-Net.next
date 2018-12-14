7e7b62b14f32b8c6cad6fe9ae2bb519287710e8a
hsfzxjy <hsfzxjy@163.com>
2018-12-14 17:08:47 +0800

    p

--- 1214-after.sh
+++ 1214-after.sh
@@ -24,6 +24,11 @@ nohup python train.py --root data -s market1501 -t market1501 -j 4 --height 256 
 
 nohup python train.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 40 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_fc512_fd_a_nohead_dan_cam_pam_head --save-dir 1214_log/densenet121_fc512_fd_a_nohead_dan_cam_pam_head_xent_40_10 --gpu-devices 6 --criterion xent  &
 
+beta=1e-6 nohup python train_reg.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 40 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_none_nohead_dan_cam_pam_head --save-dir 1214_log/densenet121_nofc_fd_none_nohead_dan_cam_pam_head_xent_reg_svdo_1e-6_40_10 --gpu-devices 7 --criterion xent --regularizer svdo --switch-loss 15 &
+
+beta=1e-6 nohup python train_reg.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 40 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_none_nohead_dan_cam_pam_nohead --save-dir 1214_log/densenet121_nofc_fd_none_nohead_dan_cam_pam_nohead_xent_reg_svdo_1e-6_40_10 --gpu-devices 7 --criterion xent --regularizer svdo --switch-loss 15 &
+
+
 sleep 160m
 
 # densenet121 fc512 cam pam singuar
@@ -62,6 +67,3 @@ beta=1e-3 nohup python train_reg.py --root data -s market1501 -t market1501 -j 4
 
 sleep 160m
 
-beta=1e-6 nohup python train_reg.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 40 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_none_nohead_dan_cam_pam_head --save-dir 1214_log/densenet121_nofc_fd_none_nohead_dan_cam_pam_head_xent_reg_svdo_1e-6_40_10 --gpu-devices 5 --criterion xent --regularizer svdo --switch-loss 15 &
-
-beta=1e-6 nohup python train_reg.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 40 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_none_nohead_dan_cam_pam_nohead --save-dir 1214_log/densenet121_nofc_fd_none_nohead_dan_cam_pam_nohead_xent_reg_svdo_1e-6_40_10 --gpu-devices 5 --criterion xent --regularizer svdo --switch-loss 15 &

c0c1713c95001f3b924a05901e550f9a82fb8c99
hsfzxjy <hsfzxjy@163.com>
2018-12-19 16:18:45 +0800

    p

--- 1219.sh
+++ 1219.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-nohup python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 60 --stepsize 20 40 --fixbase-epoch 10 --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_fc512_fd_none_nohead_dan_none_nohead --save-dir htri_log/densenet121_fc512_fd_none_nohead_dan_none_nohead_htri_60_10 --gpu-devices 0 --criterion htri &
-
-nohup python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 60 --stepsize 20 40  --open-layers classifier fc  --train-batch-size 32 --test-batch-size 100 -a densenet121_fc512_fd_none_nohead_dan_none_nohead --save-dir htri_log/densenet121_fc512_fd_none_nohead_dan_none_nohead_htri_60_0 --gpu-devices 0 --criterion htri &
-
----
-
 nohup python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 60 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 10  --train-batch-size 32 --test-batch-size 100 -a densenet121_fc512_fd_ab_nohead_dan_cam_pam_nohead --save-dir htri_log/densenet121_fc512_fd_ab_nohead_dan_cam_pam_nohead_htri_60_10 --gpu-devices 0 --criterion htri &
 
 nohup python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 60 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 10  --train-batch-size 32 --test-batch-size 100 -a densenet121_fc512_fd_ab_nohead_dan_cam_pam_head --save-dir htri_log/densenet121_fc512_fd_ab_nohead_dan_cam_pam_head_htri_60_10 --gpu-devices 0 --criterion htri &

d72f4acda3e59322e76cc0f5d56325ec5649eb7d
hsfzxjy <hsfzxjy@163.com>
2019-01-11 12:59:46 +0800

    p

--- torchreid/models/resnet.py
+++ torchreid/models/resnet.py
@@ -77,7 +77,7 @@ class Bottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
         self.downsample = downsample
         self.stride = stride
 

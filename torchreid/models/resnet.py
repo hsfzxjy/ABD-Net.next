20658e46ab31e970614c587f9a5dc9c008702409
hsfzxjy <hsfzxjy@163.com>
2019-01-11 12:43:12 +0800

    p

--- torchreid/models/resnet.py
+++ torchreid/models/resnet.py
@@ -129,7 +129,7 @@ class ResNet(nn.Module):
         # backbone network
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=False)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
@@ -296,7 +296,7 @@ class ResNet(nn.Module):
         v = self.global_avgpool(f)
         v = v.view(v.size(0), -1)
 
-        v = torch.cat([v, *attention_parts], 1)
+        # v = torch.cat([v, *attention_parts], 1)
 
         v_before_fc = v
         if self.fc is not None:

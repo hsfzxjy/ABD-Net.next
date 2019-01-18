450b93697592d6ac098d61d92eea978f34e2d4df
hsfzxjy <hsfzxjy@163.com>
2019-01-18 17:58:21 +0800

    p

--- vis/runvis.py
+++ vis/runvis.py
@@ -8,7 +8,7 @@ def get_param(input_fn, layer):
     _, dir, name = input_fn.split('/')
     os.makedirs(osp.join('with_orth_vis_output', dir, name), 0o777, True)
     path = osp.join('with_orth_vis_output', dir, name)
-    a = ['-i', osp.abspath(input_fn), '-p', osp.abspath(path), '-l', str(layer), '-w', '../log/densenet121_fc512_fd_none_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__0//checkpoint_ep60.pth.tar']
+    a = ['-i', osp.abspath(input_fn), '-p', osp.abspath(path), '-l', str(layer), '-w', '../log/densenet121_fc512_fd_none_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__0/checkpoint_ep60.pth.tar']
     # _, dir, name = input_fn.split('/')
     # os.makedirs(osp.join('without_orth_vis_output', dir, name), 0o777, True)
     # path = osp.join('without_orth_vis_output', dir, name)

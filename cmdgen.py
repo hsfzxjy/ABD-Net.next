39b02ea91142517b412cd3bd2376b3f062ddebc3
hsfzxjy <hsfzxjy@163.com>
2019-01-19 00:10:11 +0800

    p

--- cmdgen.py
+++ cmdgen.py
@@ -22,14 +22,22 @@ if __name__ == '__main__':
     parser.add_argument('--sl', type=int, default=0)
     parser.add_argument('--fcl', action='store_true', default=False)
     parser.add_argument('--dir', default='log')
+    parser.add_argument('--skip', type=str, default='')
 
     options = parser.parse_args()
 
     template = '''sing_beta={sing_beta} beta={beta} nohup python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height {size} --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 60 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 10  --train-batch-size 32 --test-batch-size 100 -a {model} --save-dir {dir}/{model}__crit_{crit}__sb_{sing_beta}__b_{beta}__sl_{sl}__fcl_{fcl}__reg_{reg}__dropout_{dropout}__dau_{dau}__pp_{pp}__size_{size}__{index} --gpu-devices {gpu} --criterion {crit} {fcl_command} --switch-loss {sl} --regularizer {reg} --dropout {dropout} --data-augment {dau} --penalty-position {pp} &'''
 
+    gpu_start = options.gpu_start
+
     for index in range(options.num):
 
-        gpu = options.gpu_start + index
+        gpu = (gpu_start + index) % 8
+
+        if str(gpu) in options.skip.split(','):
+            gpu_start += 1
+            gpu = (gpu + 1) % 8
+
         args = {
             'sing_beta': options.sing_beta,
             'beta': options.beta,

e22acb577768e71fb8f83c7a609e14c917c0052e
hsfzxjy <hsfzxjy@163.com>
2019-01-19 22:58:39 +0800

    p

--- vis/runvis.py
+++ vis/runvis.py
@@ -6,7 +6,7 @@ import os.path as osp
 def get_param(input_fn, layer):
 
     _, dir, name = input_fn.split('/')
-    os.makedirs(osp.join('densenet121_fc512_vis_output', dir, name), 0o777, True)
+    os.makedirs(osp.join('densenet121_fc512_vis_output', 'layer_' + str(layer), dir, name), 0o777, True)
     path = osp.join('densenet121_fc512_vis_output', dir, name)
     a = ['-i', osp.abspath(input_fn), '-p', osp.abspath(path), '-l', str(layer), '-w', 'log/densenet121_fc512_fd_none_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__0/checkpoint_ep60.pth.tar']
     # _, dir, name = input_fn.split('/')
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     #     params.append(get_param(fn, 5))
     for fn in glob.glob('vis_input/**/*'):
         params.extend(get_param(fn, 4))
+    for fn in glob.glob('vis_input/**/*'):
         params.extend(get_param(fn, 5))
 
     splitted = [params[i:i + 5] for i in range(0, len(params), 5)]

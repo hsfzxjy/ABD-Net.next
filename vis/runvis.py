3f6faabb3e3f9b1daffba56faee1a6429d7bcf80
hsfzxjy <hsfzxjy@163.com>
2019-01-21 20:34:59 +0800

    p

--- vis/runvis.py
+++ vis/runvis.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     # for fn in glob.glob('vis_input/**/*'):
     #     params.append(get_param(fn, 5))
     for fn in glob.glob('vis_input/**/*'):
-        params.extend(get_param(fn, 4))
+        params.extend(get_param(fn, 5))
     # for fn in glob.glob('vis_input/**/*'):
     #     params.extend(get_param(fn, 5))
 
--- vis/vis_10.py
+++ vis/vis_10.py
@@ -5,4 +5,4 @@ CD = osp.abspath(osp.dirname(__file__))
 
 for x in range(10):
 
-    os.system(f'python3 vis.py -i {CD}/pic/{x}.jpg -p {CD}/generated/{x}.jpg/ -l 5')
+    os.system(f'python3 vis.py -i {CD}/pic/{x}.jpg -p {CD}/generated/{x}.jpg/ -l 4')

249055d5b5e7aed3c46ac8484ccba92f34a3a65f
hsfzxjy <hsfzxjy@163.com>
2019-01-22 13:38:48 +0800

    p

--- torchreid/models/tricks/feature_distilation.py
+++ torchreid/models/tricks/feature_distilation.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from .attention import get_attention_module_instance, CAM_Module
+from .attention import get_attention_module_instance, CAM_Module, DANetHead
 
 
 class FeatureDistilationTrick(nn.Module):
@@ -29,7 +29,10 @@ class FeatureDistilationTrick(nn.Module):
                 cs.extend(channels[key])
             cs.sort()
 
-            cam_module = CAM_Module(len(cs))
+            if use_conv_head:
+                cam_module = DANetHead(len(cs), len(cs), nn.BatchNorm2d, CAM_Module)
+            else:
+                cam_module = CAM_Module(len(cs))
             setattr(self, f'_cam_module_{part}', cam_module)  # force gpu
 
             self.cam_modules.append((cs, cam_module))

5a5959a194f43fdd11d5ba14b967e7b533f24886
hsfzxjy <hsfzxjy@163.com>
2019-01-27 23:56:40 +0800

    p

---
+++ 0128.sh
@@ -0,0 +1,41 @@
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_c_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_c_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep60__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep60.pth.tar
+
+sleep 50m
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep60__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep60.pth.tar
+
+sleep 50m
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_a_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_a_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep60__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep60.pth.tar
+
+sleep 50m
+
+
+#
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_c_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_c_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep40__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep40.pth.tar
+
+sleep 50m
+
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep40__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep40.pth.tar
+
+sleep 50m
+
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_a_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_a_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep40__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep40.pth.tar
+
+sleep 50m
+
+
+#
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_c_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_c_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep15__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep15.pth.tar
+
+sleep 50m
+
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_ab_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_ab_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep15__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep15.pth.tar
+
+sleep 50m
+
+
+sing_beta= beta= python train_reg_crit.py --root data -s market1501 -t market1501 -j 4 --height 256 --width 128 --optim adam --label-smooth --lr 0.0003 --max-epoch 30 --eval-freq 5 --stepsize 20 40  --open-layers classifier fc --fixbase-epoch 0  --train-batch-size 32 --test-batch-size 100 -a densenet121_nofc_fd_a_nohead_dan_none_nohead --save-dir 0128_fd_log/densenet121_nofc_fd_a_nohead_dan_none_nohead__crit_xent__sb___b___sl_0__fcl_False__reg_none__dropout_none__dau_crop__pp_before__size_256__start_ep15__0 --gpu-devices 7 --criterion xent  --switch-loss 0 --regularizer none --dropout none --data-augment crop --penalty-position before --resume layer5_log/densenet121_nofc_fd_none_nohead_dan_none_nohead__crit_xent__sb_1e-7__b___sl_-15__fcl_True__reg_none__dropout_none__dau_crop__pp_layer5__size_256__open_fd__0/checkpoint_ep15.pth.tar

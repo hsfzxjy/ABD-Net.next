4302395964b21460ce8fcbffa225e35684a300d2
hsfzxjy <hsfzxjy@163.com>
2019-02-17 11:38:29 +0800

    p

--- vis/pytorch-cnn-visualization/src/misc_functions.py
+++ vis/pytorch-cnn-visualization/src/misc_functions.py
@@ -59,21 +59,21 @@ def save_class_activation_on_image(org_img, activation_map, file_name):
         activation_map (numpy arr): activation map (grayscale) 0-255
         file_name (str): File name of the exported image
     """
-    if not os.path.exists('../results'):
-        os.makedirs('../results')
+    if not os.path.exists('vis/results'):
+        os.makedirs('vis/results')
     # Grayscale activation map
-    path_to_file = os.path.join('../results', file_name+'_Cam_Grayscale.jpg')
+    path_to_file = os.path.join('vis/results', file_name+'_Cam_Grayscale.jpg')
     cv2.imwrite(path_to_file, activation_map)
     # Heatmap of activation map
     activation_heatmap = cv2.applyColorMap(activation_map, cv2.COLORMAP_HSV)
-    path_to_file = os.path.join('../results', file_name+'_Cam_Heatmap.jpg')
+    path_to_file = os.path.join('vis/results', file_name+'_Cam_Heatmap.jpg')
     cv2.imwrite(path_to_file, activation_heatmap)
     # Heatmap on picture
     print(org_img.shape)
     org_img = cv2.resize(org_img, (384, 128))
     img_with_heatmap = np.float32(activation_heatmap) + np.float32(org_img)
     img_with_heatmap = img_with_heatmap / np.max(img_with_heatmap)
-    path_to_file = os.path.join('../results', file_name+'_Cam_On_Image.jpg')
+    path_to_file = os.path.join('vis/results', file_name+'_Cam_On_Image.jpg')
     cv2.imwrite(path_to_file, np.uint8(255 * img_with_heatmap))
 
 

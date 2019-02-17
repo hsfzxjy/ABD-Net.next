394060a17df0843761d919f92bba4194a7ec2759
hsfzxjy <hsfzxjy@163.com>
2019-02-17 11:27:59 +0800

    p

--- vis/pytorch-cnn-visualization/src/misc_functions.py
+++ vis/pytorch-cnn-visualization/src/misc_functions.py
@@ -69,7 +69,7 @@ def save_class_activation_on_image(org_img, activation_map, file_name):
     path_to_file = os.path.join('../results', file_name+'_Cam_Heatmap.jpg')
     cv2.imwrite(path_to_file, activation_heatmap)
     # Heatmap on picture
-    org_img = cv2.resize(org_img, (224, 224))
+    org_img = cv2.resize(org_img, (384, 128))
     img_with_heatmap = np.float32(activation_heatmap) + np.float32(org_img)
     img_with_heatmap = img_with_heatmap / np.max(img_with_heatmap)
     path_to_file = os.path.join('../results', file_name+'_Cam_On_Image.jpg')

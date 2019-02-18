72b33e1e63375c3441fe5307dc3e6888921b5ea6
hsfzxjy <hsfzxjy@163.com>
2019-02-18 17:52:40 +0800

    p

--- torchreid/models/tricks/attention.py
+++ torchreid/models/tricks/attention.py
@@ -166,7 +166,7 @@ class CAM_Module(Module):
         super(CAM_Module, self).__init__()
         self.channel_in = in_dim
 
-        self.register_buffer('gamma', Parameter(torch.zeros(1)))
+        self.register_buffer('gamma', Parameter(torch.zeros(1).cuda()))
         print(self.gamma.device)
         self.softmax = Softmax(dim=-1)
 

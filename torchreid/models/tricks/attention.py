83b29865a5e7b9905eabe28ab72a74a20c302a14
hsfzxjy <hsfzxjy@163.com>
2019-02-18 17:37:58 +0800

    p

--- torchreid/models/tricks/attention.py
+++ torchreid/models/tricks/attention.py
@@ -166,8 +166,7 @@ class CAM_Module(Module):
         super(CAM_Module, self).__init__()
         self.channel_in = in_dim
 
-        self.gamma = Parameter(torch.zeros(1))
-        self.register_parameter('gamma', self.gamma)
+        self.register_parameter('gamma', Parameter(torch.zeros(1)))
         self.softmax = Softmax(dim=-1)
 
     def forward(self, x):

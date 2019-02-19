21302f0e7bb3dea7f4d49701c2f15769a1111b6b
hsfzxjy <hsfzxjy@163.com>
2019-02-19 20:40:02 +0800

    p

--- torchreid/models/resnet.py
+++ torchreid/models/resnet.py
@@ -860,6 +860,317 @@ def init_pretrained_weights(model, model_url):
     print("Initialized model with pretrained weights from {}".format(model_url))
 
 
+class ResNetTr8(nn.Module):
+    """
+    Residual network
+
+    Reference:
+    He et al. Deep Residual Learning for Image Recognition. CVPR 2016.
+    """
+
+    def __init__(self, num_classes,
+                 last_stride=2,
+                 fc_dims=None,
+                 dropout_p=None,
+                 *,
+                 fd_config=None,
+                 attention_config=None,
+                 dropout_optimizer=None,
+                 sum_fusion: bool=False,
+                 tricky: int=0,
+                 **kwargs):
+
+        self.sum_fusion = True
+        self.tricky = 8
+
+        assert self.sum_fusion and fc_dims
+
+        # self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.feature_dim = 2048  # 512 * block.expansion
+
+        # backbone network
+        backbone = ResNetBackbone(Bottleneck, [3, 4, 6, 3], last_stride)
+        init_pretrained_weights(backbone, model_urls['resnet50'])
+        self.conv1 = backbone.conv1
+        self.bn1 = backbone.bn1
+        self.relu = backbone.relu
+        self.maxpool = backbone.maxpool
+        self.layer1 = backbone.layer1
+        self.layer2 = backbone.layer2
+        self.layer3 = backbone.layer3
+        self.layer4 = backbone.layer4
+
+        normal_branch_stride = 1
+        self.dummy_sum = DummySum()
+
+        self.layer4_normal_branch = nn.Sequential(
+            Bottleneck(
+                1024,
+                512,
+                stride=normal_branch_stride,
+                downsample=nn.Sequential(
+                    nn.Conv2d(
+                        1024, 2048, kernel_size=1, stride=normal_branch_stride, bias=False
+                    ),
+                    nn.BatchNorm2d(2048)
+                )
+            ),
+            Bottleneck(2048, 512),
+            Bottleneck(2048, 512)
+        )
+        self.layer4_normal_branch.load_state_dict(backbone.layer4.state_dict())
+
+        # Begin Feature Distilation
+        if fd_config is None:
+            fd_config = {'parts': (), 'use_conv_head': False}
+        from .tricks.feature_distilation import FeatureDistilationTrick
+        self.feature_distilation = FeatureDistilationTrick(
+            fd_config['parts'],
+            channels=channels,
+            use_conv_head=fd_config['use_conv_head']
+        )
+        self._init_params(self.feature_distilation)
+        self.dummy_fd = DummyFD(lambda: self.feature_distilation)
+        # End Feature Distilation
+
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
+
+        num_features = 2048
+        # Begin Attention Module
+        self.get_tricky_8_attention_module()
+        # End Attention Module
+
+        # Begin Dropout Module
+        if dropout_optimizer is None:
+            from .tricks.dropout import SimpleDropoutOptimizer
+            dropout_optimizer = SimpleDropoutOptimizer(dropout_p)
+        # End Dropout Module
+
+        self.fc = self._construct_fc_layer(fc_dims, num_features, dropout_optimizer)
+        self.classifier = nn.Linear(self.feature_dim, num_classes, bias=False)
+
+        self.reduction_tr = nn.Sequential(
+            nn.Conv2d(2048, 1024, kernel_size=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            *dropout
+        )
+
+        self.classifier_before = nn.Linear(256, num_classes, bias=False)
+        self.classifier_after = nn.Linear(256, num_classes, bias=False)
+        self.classifier_pam = nn.Linear(256, num_classes, bias=False)
+        self.classifier_cam = nn.Linear(256, num_classes, bias=False)
+
+        self._init_params(self.classifier_after)
+        self._init_params(self.classifier_before)
+        self._init_params(self.classifier_cam)
+        self._init_params(self.classifier_pam)
+
+        self._init_params(self.fc)
+        self._init_params(self.classifier)
+
+    def get_tricky_8_attention_module(self):
+
+        from .tricks.attention import DANetHead, CAM_Module, PAM_Module
+
+        in_channels = 1024
+        out_channels = 256
+
+        self.before_module = DANetHead(in_channels, out_channels, nn.BatchNorm2d, lambda _: lambda x: x)
+        self.pam_module = DANetHead(in_channels, out_channels, nn.BatchNorm2d, PAM_Module)
+        self.cam_module = DANetHead(in_channels, out_channels, nn.BatchNorm2d, CAM_Module)
+        self.sum_conv = nn.Sequential(nn.Dropout2d(0.1, False), nn.Conv2d(out_channels, out_channels, 1))
+
+        self._init_params(self.before_module)
+        self._init_params(self.cam_module)
+        self._init_params(self.pam_module)
+        self._init_params(self.sum_conv)
+
+    def backbone_convs(self):
+
+        convs = [
+            self.conv1,
+            self.bn1,
+            self.relu,
+            self.maxpool,
+            self.layer1,
+            self.layer2,
+            self.layer3,
+            self.layer4,
+        ]
+
+        if hasattr(self, 'layer4_normal_branch'):
+            convs.append(self.layer4_normal_branch)
+        if hasattr(self, 'layer4_si'):
+            convs.append(self.layer4_si)
+
+        return convs
+
+    def forward_feature_distilation(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+
+        B, C, H, W = x.shape
+
+        for cs, cam in self.feature_distilation.cam_modules:
+            c_tensor = torch.tensor(cs).cuda()
+
+            new_x = x[:, c_tensor]
+            new_x = cam(new_x)
+            x[:, c_tensor] = new_x
+
+        layer5 = x
+
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x, layer5
+
+    def _construct_fc_layer(self, fc_dims, input_dim, dropout_optimizer):
+        """
+        Construct fully connected layer
+
+        - fc_dims (list or tuple): dimensions of fc layers, if None,
+                                   no fc layers are constructed
+        - input_dim (int): input dimension
+        - dropout_p (float): dropout probability, if None, dropout is unused
+        """
+
+        if fc_dims is None:
+            self.feature_dim = input_dim
+            return None
+
+        assert isinstance(fc_dims, (list, tuple)), "fc_dims must be either list or tuple, but got {}".format(type(fc_dims))
+
+        layers = []
+
+        for dim in fc_dims:
+            layers.append(nn.Linear(input_dim, dim))
+            layers.append(nn.BatchNorm1d(dim))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(dropout_optimizer)
+            # if dropout_p is not None:
+            #     layers.append(nn.Dropout(p=dropout_p))
+            input_dim = dim
+
+        self.feature_dim = fc_dims[-1]
+
+        return nn.Sequential(*layers)
+
+    def _init_params(self, x):
+        if x is None:
+            return
+
+        for m in x.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight, 1, 0.02)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.normal_(m.weight, 1, 0.02)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+
+        B, C, H, W = x.shape
+
+        for cs, cam in self.feature_distilation.cam_modules:
+            c_tensor = torch.tensor(cs).cuda()
+
+            new_x = x[:, c_tensor]
+            new_x = cam(new_x)
+            x[:, c_tensor] = new_x
+
+        layer5 = x
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        triplet_features = []
+        xent_features = []
+        predict_features = []
+
+        # normal branch
+        x1 = x
+        x1 = self.layer4_normal_branch(x1)
+        x1 = self.global_avgpool(x1)
+        x1 = x1.view(x1.size(0), -1)
+        triplet_features.append(x1)
+        x1 = self.fc(x1)
+        predict_features.append(x1)
+        x1 = self.classifier(x1)
+        xent_features.append(x1)
+
+        # our branch
+        x2 = x
+        f = self.layer4(x2)
+        f = self.reduction_tr(f)
+
+        f_before = self.before_module(f)
+        f_cam = self.cam_module(f)
+        f_pam = self.pam_module(f)
+
+        f_sum = f_cam + f_pam + f_before
+        f_after = self.sum_conv(f_sum)
+        feature_dict = {
+            'cam': f_cam,
+            'before': f_before,
+            'pam': f_pam,
+            'after': f_after,
+            'layer5': layer5,
+        }
+
+        v = self.global_avgpool(f_before)
+        v = v.view(v.size(0), -1)
+        triplet_features.append(v)
+        predict_features.append(v)
+        v = self.classifier_before(v)
+        xent_features.append(v)
+
+        v = self.global_avgpool(f_pam)
+        v = v.view(v.size(0), -1)
+        triplet_features.append(v)
+        predict_features.append(v)
+        v = self.classifier_pam(v)
+        xent_features.append(v)
+
+        v = self.global_avgpool(f_cam)
+        v = v.view(v.size(0), -1)
+        triplet_features.append(v)
+        predict_features.append(v)
+        v = self.classifier_cam(v)
+        xent_features.append(v)
+
+        v = self.global_avgpool(f_after)
+        v = v.view(v.size(0), -1)
+        triplet_features.append(v)
+        predict_features.append(v)
+        v = self.classifier_after(v)
+        xent_features.append(v)
+
+        if not self.training:
+            return torch.cat(predict_features, 1)
+
+        return None, tuple(xent_features), tuple(triplet_features), feature_dict
+
+
 """
 Residual network configurations:
 --
@@ -927,11 +1238,11 @@ def make_function_sf_ls1_50(name, config):
     globals()[name] = _func
 
 
-def make_function_sf_tricky_50(name, config, tricky):
+def make_function_sf_tricky_50(name, config, tricky, base_class=ResNet):
 
     def _func(num_classes, loss, pretrained='imagenet', **kwargs):
         print(config)
-        return ResNet(
+        return base_class(
             num_classes=num_classes,
             last_stride=2,
             dropout_p=None,
@@ -1051,3 +1362,13 @@ for tricky in range(1, 12):
             config.update({key: sub_config})
 
         make_function_sf_tricky_50(name, config, tricky)
+
+for fragment in fragments:
+
+    name = f'resnet50_sf_tr8'
+    config = {}
+    for key, (sub_config, name_frag) in zip(keys, fragment):
+        name += name_frag
+        config.update({key: sub_config})
+
+    make_function_sf_tricky_50(name, config, 8, base_class=ResNetTr8)

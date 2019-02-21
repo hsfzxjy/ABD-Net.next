b46eaf32c1ed8984d3670e87a677470c874a3e29
hsfzxjy <hsfzxjy@163.com>
2019-02-21 23:35:45 +0800

    p

---
+++ 0222.sh
@@ -0,0 +1,5 @@
+sleep 420m
+
+ps aux | grep train_reg | awk '{system("kill " $2)}'
+
+head_col=2 reg_const=1 flip_eval=1 dropout_reduction=1 sing_beta=1e-6 beta=1e-3 nohup python train_reg_crit.py --root data -s market1501 -t market1501  -j 4 --height 384 --width 128 --eval-freq 1 --optim adam --label-smooth --lr 0.0003 --max-epoch 200 --stepsize 20 40  --open-layers classifier fc before_module1 pam_module1 cam_module1 sum_conv1 --fixbase-epoch 10  --train-batch-size 128 --test-batch-size 100 -a resnet50_sf_tr9_fc1024_fd_ab_c_nohead_dan_cam_pam_nohead --save-dir 0221_trick_log/resnet50_sf_tr9_fc1024_fd_ab_c_nohead_dan_cam_pam_nohead__crit_singular__htri_sb_1e-6__b_1e-3__sl_-176__fcl_False__reg_const_svmo__dropout_incr__dau_crop,random-erase__pp_before,after,cam,pam,layer5__size_384__ep_200__lh_.1__dr__b128__m1.2__hc_2_1 --gpu-devices 0,1,2,3,4 --criterion singular_htri  --switch-loss -176 --regularizer svmo --dropout incr --data-augment crop,random-erase  --penalty-position before,after,cam,pam,layer5  --lambda-htri 0.1 --margin 1.2 &

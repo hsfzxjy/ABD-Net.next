25feae3ea6e5290797b824207676055c324a0edb
hsfzxjy <hsfzxjy@163.com>
2019-02-23 22:27:20 +0800

    p

--- torchreid/losses/wrapped_triplet_loss.py
+++ torchreid/losses/wrapped_triplet_loss.py
@@ -1,3 +1,6 @@
+import os
+import torch
+
 from .hard_mine_triplet_loss import TripletLoss
 from .cross_entropy_loss import CrossEntropyLoss
 
@@ -9,7 +12,19 @@ def WrappedTripletLoss(num_classes: int, use_gpu: bool, args, param_controller, 
 
     def _loss(x, pids):
 
-        _, y, v, _ = x
+        _, y, v, features_dict = x
+
+        if os.environ.get('sa') is not None:
+            layer3, layer4_1, layer4_2 = features_dict['layers']
+
+            layer3 = torch.norm(layer3, dim=1, p=2) ** 2 / 1024
+            layer4_1 = torch.norm(layer4_1, dim=1, p=2) ** 2 / 2048
+            layer4_2 = torch.norm(layer4_2, dim=1, p=2) ** 2 / 2048
+
+            as_loss = ((layer3 - layer4_1) ** 2).sum() + ((layer3 - layer4_2) ** 2).sum()
+            print(as_loss)
+        else:
+            as_loss = 0.
 
         if htri_only:
             loss = htri_loss(y, pids)

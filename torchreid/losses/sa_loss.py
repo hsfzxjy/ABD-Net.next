0ee213a42503d6c2c26a317861f02bdecde02006
hsfzxjy <hsfzxjy@163.com>
2019-02-24 10:56:13 +0800

    p

--- torchreid/losses/sa_loss.py
+++ torchreid/losses/sa_loss.py
@@ -11,10 +11,10 @@ def sa_loss(features_dict):
         layer3 = layer3.view(layer3.size(0), -1)
         layer4_1 = torch.norm(layer4_1, dim=1, p=2) ** 2 / 2048
         layer4_1 = layer4_1.view(layer4_1.size(0), -1)
-        layer4_2 = torch.norm(layer4_2, dim=1, p=2) ** 2 / 2048
-        layer4_2 = layer4_2.view(layer4_2.size(0), -1)
+        # layer4_2 = torch.norm(layer4_2, dim=1, p=2) ** 2 / 2048
+        # layer4_2 = layer4_2.view(layer4_2.size(0), -1)
 
-        as_loss = (((layer3 - layer4_1) ** 2).sum() + ((layer3 - layer4_2) ** 2).sum()) * .1
+        as_loss = (((layer3 - layer4_1) ** 2).sum()) * .1
         print(as_loss)
     else:
         as_loss = 0.

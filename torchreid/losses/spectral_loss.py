82fd7ecfe7315c5c610980b2c128d74f01bc1ac5
hsfzxjy <hsfzxjy@163.com>
2019-03-05 18:34:25 +0800

    p

--- torchreid/losses/spectral_loss.py
+++ torchreid/losses/spectral_loss.py
@@ -8,7 +8,7 @@ from .cross_entropy_loss import CrossEntropyLoss
 def btr(A: 'N x C x C'):
 
     N, C, _ = A.size()
-    A = torch.sqrt(torch.bmm(A, A))
+    A = torch.sqrt(torch.bmm(A, A.permute(0, 2, 1)))
     eye = torch.eye(C, device='cuda').expand(N, C, C).view(N, C * C, 1)
     return torch.bmm(A.view(N, 1, C * C), eye).view(N)
 

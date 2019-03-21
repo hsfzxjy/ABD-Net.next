e881223f50bc5cef7388842963b92686717e4485
hsfzxjy <hsfzxjy@163.com>
2019-03-21 17:04:30 +0800

    p

--- tsne.py
+++ tsne.py
@@ -1,10 +1,15 @@
 from MulticoreTSNE import MulticoreTSNE as TSNE
 import scipy.io as io
-
+import numpy as np
+from numpy.linalg import norm
 
 mat = io.loadmat('fuck.mat')
 t = mat['gt']
 tsne = TSNE(n_jobs=32)
+
+features = mat['g']
+features = features / (norm(features, p=2, dim=1, keepdim=True))
+
 a = tsne.fit_transform(mat['g'])
 
 io.savemat('aa.mat', {'a': a, 't': t})

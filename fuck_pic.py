28547f879117c089281e33b9b48be5c75dd2e5e1
hsfzxjy <hsfzxjy@163.com>
2019-03-21 21:02:09 +0800

    p

--- fuck_pic.py
+++ fuck_pic.py
@@ -30,6 +30,7 @@ def process(fn):
         #     print('\n'.join(gps))
         errors.append([
             5 - len([x for x in gpid if x == qpid]),
+            qidx,
             qp[qidx],
             gps
         ])
@@ -40,7 +41,23 @@ def process(fn):
 b_error = process('base_distmat.mat')
 f_error = process('final_distmat.mat')
 
+import os
+import shutil
+shutil.rmtree('pics')
+os.makedirs('pics')
+
 for be, fe in zip(b_error, f_error):
 
+    qidx = be[1]
+    qf = be[2]
+
     if be[0] > fe[0] + 2:
-        print(be[2], fe[2])
+        directory = 'pics/' + str(qidx)+'/'
+        os.makedirs(directory)
+        shutil.copy(qf, directory+'query.jpg')
+
+        for base, paths in [['baseline', be[3]], ['final', fe[3]]]:
+            d = directory + base + '/'
+            os.makedirs(d)
+            for x in paths:
+                shutil.copy(x, d+os.path.basename(x))

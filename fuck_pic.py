a2fa1027488ab219777ab6f2a075bf8ee1130eba
hsfzxjy <hsfzxjy@163.com>
2019-03-21 20:46:52 +0800

    p

--- fuck_pic.py
+++ fuck_pic.py
@@ -18,6 +18,8 @@ def process(fn):
     indices = np.argsort(distmat, axis=1)
     print(indices)
 
+    errors = []
+
     for qidx in range(len(qp)):
         print('index =', qidx)
         print(qp[qidx])
@@ -26,6 +28,19 @@ def process(fn):
         gpid = [pid(x) for x in gps]
         if len([x for x in gpid if x == qpid]) < 5:
             print('\n'.join(gps))
+        errors.append([
+            5 - len([x for x in gpid if x == qpid]),
+            qp[qidx],
+            gps
+        ])
 
+    return errors
 
-process('base_distmat.mat')
+b_error = process('base_distmat.mat')
+f_error = process('final_distmat.mat')
+
+for be, fe in zip(b_error, f_error):
+
+    if be[0] > fe[0]:
+        print(be[2], fe[2])
+

a99751aadb0367b691482adcb84eec8a16c2ea3f
hsfzxjy <hsfzxjy@163.com>
2019-03-21 20:29:34 +0800

    p

---
+++ fuck_pic.py
@@ -0,0 +1,17 @@
+import scipy.io as io
+import numpy as np
+
+
+def process(fn):
+
+    dct = io.loadmat(fn)
+    distmat = dct['distmat']
+    qp = dct[qp]
+    gp = dct[gp]
+
+    indices = np.argsort(distmat, axis=1)
+    matches = (gp[indices] == qp[:, np.newaxis]).astype(np.int32)
+    print(matches)
+    print(matches.shape)
+
+process('base_distmat.mat')

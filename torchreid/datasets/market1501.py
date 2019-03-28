56f206dadf807427ce4f201b3cd7c822801cb1c1
hsfzxjy <hsfzxjy@163.com>
2019-03-28 19:43:14 +0800

    p

--- torchreid/datasets/market1501.py
+++ torchreid/datasets/market1501.py
@@ -84,7 +84,7 @@ class Market1501(BaseImageDataset):
         for img_path in img_paths:
             pid, camid = map(int, pattern.search(img_path).groups())
             if pid == -1 and os.environ.get('junk') is None: continue  # junk images are just ignored
-            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert -1 <= pid <= 1501  # pid == 0 means background
             assert 1 <= camid <= 6
             camid -= 1 # index starts from 0
             if relabel: pid = pid2label[pid]

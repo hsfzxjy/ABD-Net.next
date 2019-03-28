02871f6b9cdcf495a469a442bc32e61f2c650a3d
hsfzxjy <hsfzxjy@163.com>
2019-03-28 19:54:38 +0800

    p

--- fuck_pic.py
+++ fuck_pic.py
@@ -54,9 +54,9 @@ for be, fe, de in zip(b_error, f_error, d_error):
     qidx = be[1]
     qf = be[2]
 
-    if -1 in be[3] + fe[3] + de[3]:  # be[0] > fe[0] + 2:
+    if '-1' in be[3] + fe[3] + de[3]:  # be[0] > fe[0] + 2:
         print(qidx, be[0], de[0], fe[0])
-        lines.append('{} {} {} {}'.format(qidx, be[0], de[0], fe[0]))
+        lines.append('{} {} {} {}'.format(qidx, [x for x in be[3] if x == '-1'], [x for x in de[3] if x == '-1'], [x for x in fe[0] if x == '-1']))
         directory = 'pics/' + str(qidx) + '/'
         os.makedirs(directory)
         shutil.copy(qf.strip(), directory + 'query.jpg')

a55c73c9a0350a76d13a75fd54e7f9cfc5462209
hsfzxjy <hsfzxjy@163.com>
2019-04-10 16:30:08 +0800

    p

--- extract_softmax.py
+++ extract_softmax.py
@@ -183,8 +183,8 @@ def extract_train_info(model, trainloader):
 
     with open('softmax_results.csv', 'w') as f:
         f.write('id global_score global_correct p1_score p1_correct p2_score p2_correct\n'.replace(' ', ','))
-        for i, xs in enumerate(zip([score_list[0], correct_list[0], score_list[1], correct_list[1], score_list[2], correct_list[2]])):
-            f.write(','.join(map(str, [i, *xs])))
+        for i, xs in enumerate(zip(score_list[0], correct_list[0], score_list[1], correct_list[1], score_list[2], correct_list[2])):
+            f.write(','.join(map(str, [i, *[x.item() for x in xs]])))
 
 
 def train(epoch, model, criterion, regularizer, optimizer, trainloader, use_gpu, fixbase=False, switch_loss=False):

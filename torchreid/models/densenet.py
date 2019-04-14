7773814ce947943d817c9a0f3ce2aa3e12b9040a
hsfzxjy <hsfzxjy@163.com>
2019-04-14 21:31:18 +0800

    p

--- torchreid/models/densenet.py
+++ torchreid/models/densenet.py
@@ -293,6 +293,24 @@ class DensenetABD(nn.Module):
 
         return convs
 
+    def _init_params(self, x):
+        for m in x.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+
     def get_attention_module(self):
 
         from .tricks.attention import DANetHead, CAM_Module, PAM_Module

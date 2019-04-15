3d82e4b7b0f840d6aa16391993dd18f78c9216fe
hsfzxjy <hsfzxjy@163.com>
2019-04-16 00:39:41 +0800

    p

--- vis/vis_10.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path as osp
-
-CD = osp.abspath(osp.dirname(__file__))
-
-for x in range(10):
-
-    os.system(f'python3 vis.py -i {CD}/pic/{x}.jpg -p {CD}/generated/{x}.jpg/ -l 4')

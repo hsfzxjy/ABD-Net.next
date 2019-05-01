53ce0e7e36715158c5eeb7c60779f14a8dfbd82d
hsfzxjy <hsfzxjy@163.com>
2019-05-01 21:49:33 +0800

    p

--- vis/heatmap_aicity.py
+++ vis/heatmap_aicity.py
@@ -36,6 +36,7 @@ class args:
     global_dim = 1024
     dropout = 0.5
     global_max_pooling = False
+    abd_dan_no_head = True
     compatibility = False
     split_id = 0
     cuhk03_labeled = False

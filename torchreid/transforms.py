408a41f5c391f2040197c18a93d5521c92ade6ad
hsfzxjy <hsfzxjy@163.com>
2019-05-03 00:02:21 +0800

    p

--- torchreid/transforms.py
+++ torchreid/transforms.py
@@ -142,7 +142,6 @@ def build_training_transforms(height, width, data_augment):
     imagenet_std = [0.229, 0.224, 0.225]
     normalize = Normalize(mean=imagenet_mean, std=imagenet_std)
 
-    data_augment = set(data_augment.split(','))
     print('Using augmentation:', data_augment)
 
     transforms = []

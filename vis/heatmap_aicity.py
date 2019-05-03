a1d3c7073c21ca7c32780d6268c4831a38ba89a0
hsfzxjy <hsfzxjy@163.com>
2019-05-03 21:32:53 +0800

    p

--- vis/heatmap_aicity.py
+++ vis/heatmap_aicity.py
@@ -144,6 +144,7 @@ if __name__ == '__main__':
         model = gradcam = cam = None
 
         for attrname, times, basename in [
+            ('layer1', 0, 'layer1'),
             ('layer3', 0, 'layer3'),
             ('sum_conv1', 0, 'sum_conv0'),
             # ('sum_conv1', 1, 'sum_conv1'),

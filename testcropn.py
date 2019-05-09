fb2b4cebe51cc2e240f55b5f4e0735bd01bc98c6
hsfzxjy <hsfzxjy@163.com>
2019-05-09 13:07:33 +0800

    p

--- testcropn.py
+++ testcropn.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 
-from torchreid.transforms import RandomCenterCrop
+from torchreid.transforms import RandomCenterCrop, CenterCropN
 
 rt = RandomCenterCrop(224)
+ct = CenterCropN(224, 5, 5, 2)
 
 img = Image.open('/data/VeRi/aic19/image_train/000007.jpg').convert('RGB')
 plt.imshow(img)
 plt.show()
-plt.imshow(rt(img))
+plt.imshow(ct(img))
 plt.show()

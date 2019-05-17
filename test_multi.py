3fd74b300c428de9fd2973d76c0ff4c9d65cf122
hsfzxjy <hsfzxjy@163.com>
2019-05-17 21:34:04 +0800

    p

--- test_multi.py
+++ test_multi.py
@@ -194,9 +194,8 @@ def test(model, loaders, use_gpu, ranks=[1, 5, 10, 20], return_distmat=True):
             return qf, q_pids, q_camids
 
     results = {}
-    print(loaders)
     for name, dct in loaders.items():
-        results[name] = (eval_set('{name} query'.format(name), dct['query']), eval_set('{name} gallery'.format(name), dct['gallery']))
+        results[name] = (eval_set('{} query'.format(name), dct['query']), eval_set('{} gallery'.format(name), dct['gallery']))
 
     def eval_result(gr, qr):
 

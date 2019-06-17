9688e5ce5e412d6b98e880db084e3b5c586466fe
hsfzxjy <hsfzxjy@163.com>
2019-06-17 11:05:20 +0800

    p

--- rebuttal.sh
+++ rebuttal.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --gpu-devices 0 --max-epoch 80 --save-dir cam_log/cam --arch resnet50 --branches abd --abd-dan cam --abd-np 1 &
 
-python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 0 --max-epoch 80 --save-dir cam_log/of_25 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 25 --of-position before &
-
 python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 1 --max-epoch 80 --save-dir cam_log/of_35 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 35 --of-position before &
 
 python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 1 --max-epoch 80 --save-dir cam_log/of_45 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 45 --of-position before --abd-dan cam &
 
 python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 2 --max-epoch 80 --save-dir cam_log/of_cam_25 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 25 --of-position before cam --abd-dan cam &
 
+python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 3 --max-epoch 80 --save-dir cam_log/of_cam_45 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 45 --of-position before cam --abd-dan cam &
+
+wait
+
+python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 0 --max-epoch 80 --save-dir cam_log/of_25 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 25 --of-position before &
+
+python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 1 --max-epoch 80 --save-dir cam_log/of_45 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 45 --of-position before --abd-dan cam &
+
 python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 2 --max-epoch 80 --save-dir cam_log/of_cam_35 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 35 --of-position before cam --abd-dan cam &
 
-python train.py -s market1501 -t market1501 --flip-eval --eval-freq 1 --label-smooth --criterion xent --lambda-htri 0.1 --data-augment crop random-erase --margin 1.2 --train-batch-size 64 --height 384 --width 128 --optim adam --lr 0.0003 --stepsize 20 40 --fixbase-epoch 10 --gpu-devices 3 --max-epoch 80 --save-dir cam_log/of_cam_45 --arch resnet50 --branches abd --abd-np 1 --use-of --of-beta 1e-7 --of-start-epoch 45 --of-position before cam --abd-dan cam &
 
 wait
 

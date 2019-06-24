368574adfe94e15b0155d0fc9193babb9aeb067a
hsfzxjy <hsfzxjy@163.com>
2019-06-25 00:24:47 +0800

    p

--- torchreid/data_manager.py
+++ torchreid/data_manager.py
@@ -88,7 +88,7 @@ class ImageDataManager(BaseDataManager):
                 cuhk03_classic_split=self.cuhk03_classic_split
             )
 
-            for img_path, pid, camid in dataset.train:
+            for img_path, pid, camid, *_ in dataset.train:
                 pid += self._num_train_pids
                 camid += self._num_train_cams
                 self.train.append((img_path, pid, camid))

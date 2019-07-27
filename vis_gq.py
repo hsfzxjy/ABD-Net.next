ddbb2c3be63ecb60a87f14d7bb41c6445cee755d
hsfzxjy <hsfzxjy@gmail.com>
2019-07-27 18:42:06 +0800

    p

--- vis_gq.py
+++ vis_gq.py
@@ -134,7 +134,7 @@ def generate_CAM(outputs):
     print('hi')
     cam[12:, :] = generate_map(outputs, 2)
     print(cam)
-    cam = cv2.resize(cam.transpose(), (64, 128))
+    cam = cv2.resize(cam, (64, 128))
     return np.uint8(cam * 255)
     # return cam.transpose()
 

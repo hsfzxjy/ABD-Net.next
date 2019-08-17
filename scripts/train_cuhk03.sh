de63c5d5c8ed92d425da0a058d3592d7e9ef0496
hsfzxjy <hsfzxjy@gmail.com>
2019-08-17 12:55:29 +0000

    Add CUHK03 training script.

---
+++ scripts/train_cuhk03.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ -z "$GPU" ]; then
+    GPU=0,1
+fi
+
+export GPU=$GPU
+echo Using GPU: $GPU
+
+export LOG_DIR="log/abd_best_cuhk03_"$2
+mkdir -p $LOG_DIR
+echo Logging to: $LOG_DIR
+
+cd `git rev-parse --show-toplevel`
+
+function run_script {
+    python train.py -s cuhk03 -t cuhk03 \
+        --flip-eval --eval-freq 1 \
+        --label-smooth \
+        --criterion htri \
+        --lambda-htri 0.1  \
+        --data-augment crop random-erase \
+        --margin 1.2 \
+        --train-batch-size 64 \
+        --height 384 \
+        --width 128 \
+        --optim adam --lr 0.0003 \
+        --stepsize 20 40 \
+        --gpu-devices $GPU \
+        --max-epoch 80 \
+        --save-dir $LOG_DIR \
+        --arch resnet50 \
+        --use-of \
+        --abd-dan cam pam \
+        --abd-np 2 \
+        --shallow-cam \
+        --use-ow 
+}
+export -f run_script
+
+if [ "$1"x == "train"x ]; then
+    nohup bash -c run_script > /dev/null &
+    sleep 2
+    tail -f $LOG_DIR/log_train.txt
+elif [ "$1"x == "debug"x ]; then
+    run_script
+elif [ "$1"x == "kill"x ]; then
+    ps aux | grep $LOG_DIR | awk '{system("kill " $2)}'
+elif [ "$1"x == "log"x ]; then
+    tail -f $LOG_DIR/log_train.txt
+else
+    echo Unknown sub-command: $1
+    exit 1
+fi
